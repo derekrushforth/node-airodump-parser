69d7e0607053dfe1f81c63a0c44bed2a9b327f08
Derek Rushforth <derek@wildbit.com>
2015-02-27 20:21:16 -0700

    Initial setup of parsing and watch methods

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var http = require('http'),
     _ = require('lodash'),
     app = express(),
     env = process.env.NODE_ENV || 'dev',
-    fs = require('fs');
+    fs = require('fs'),
+    watch = require('watch'),
+    request = require('request');
 
+var endpoint = 'http://requestb.in/1i5ulv01';
 
 //-------------------------------------------
 // Config
@@ -20,24 +23,54 @@ var config = {
 
 var server = app.listen(config.port, function() {
     console.log('Starting up on port: ' + config.port);
+
+    startWatching();
+    // TODO: Exec airodump cmd
+    // TODO: Start watching
 });
 
+function startWatching() {
+  console.log('Watch');
+
+  watch.createMonitor('./data', function (monitor) {
+    monitor.on("changed", function (f, curr, prev) {
+      console.log('changed');
+      console.log(curr);
+      // TODO: parse changed file
+    });
+  });
+}
+
+function parse() {
+  console.log('Parse');
+
+  fs.readFile('data/kismet.json', 'utf-8', function(err, data) {
+    if (err) throw err;
+
+    var json = JSON.parse(data);
+
+    var options = {
+      method: 'post',
+      body: json,
+      json: true,
+      url: endpoint
+    }
+
+    request(options, function(err, response, body) {
+      if (err) throw err
+      console.log(body);
+    });
+
+  });
+}
 
 
 // Routes
 //-------------------------------------------
 
 
-
-
-app.get('/t', function(req, res) {
+app.get('/test', function(req, res) {
   console.log('test');
-  res.sendStatus(200);
-});
-
-app.post('/data', function(req, res) {
-  console.log('test 2  ');
-  res.send('POST request to /data');
   res.sendStatus(200);
 });
 
